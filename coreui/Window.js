e9b2c4214ef8a0f3f9a3adb84e234aec56ecf4ba
aquilahgs <orlovsky@dev-ique.com>
2010-08-30 15:26:59 +0300

    Added suport for window toolbars

--- coreui/Window.js
+++ coreui/Window.js
@@ -1,6 +1,10 @@
 Class('iQue.UI.Window', {
   isa: iQue.UI.View
   
+, has: {
+    toolbar: { is: 'ro', required: false }
+  }
+  
 , have: {
     tiClass: 'Window'
   , tiFactory: Ti.UI.createWindow
@@ -10,6 +14,34 @@ Class('iQue.UI.Window', {
     initStrings: function () {
       this.__i18nStrings.push('title', 'titlePrompt', 'backButtonTitle');
       this.__themeStrings.push('barImage', 'titleImage', 'backButtonTitleImage');
+    }
+    
+  , render: function () {
+      var tbconf = this.origConfig.toolbar;
+      this.toolbar = { };
+      tbconf && this.tiCtrl.setToolbar(tbconf.collect(function (item, idx) {
+        if (item == '<=>') 
+          return Ti.UI.createButton({ systemButton: Ti.UI.iPhone.SystemButton.FLEXIBLE_SPACE });
+        var constructor = item.builder;
+        if (!isFunction(constructor))
+          return this.error("Toolbar button " + item.name + " does not supply proper constructor");
+        try {
+          var ctrl = new constructor(apply({ parent: this }, item));
+          this.toolbar[item.name || idx] = ctrl;
+          return ctrl.tiCtrl;
+        } catch (ex) {
+          this.error("Exception during component build process:");
+          this.error(ex);
+          return null;
+        }
+      }, this).compact());
+    }
+  }
+
+, override: {
+    iqueAxis: function (item) {
+      if (item.startsWith('=')) return this.toolbar;
+      else return this.SUPER(item);
     }
   }
 
