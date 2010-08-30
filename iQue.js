688b4b51e60724cc1eaac2ca998347ff4d1d82d1
aquilahgs <orlovsky@dev-ique.com>
2010-08-30 20:01:53 +0300

    Added contact creation function shortcut

--- iQue.js
+++ iQue.js
@@ -40,6 +40,33 @@ iQue = {
 , openYouTube: function (ytid) {
     Ti.Platform.openURL('http://youtube.com/watch?v=' + ytid);
   }
+, addContact: function (details) {
+    var person = Ti.Contacts.createPerson(details);
+    details.kind && (person.kind = details.kind);
+
+    details.birthday && (person.birthday = details.birthday);
+    details.firstName && (person.firstName = details.firstName);
+    details.lastName && (person.lastName = details.lastName);
+    details.middleName && (person.middleName = details.middleName);
+    details.nickname && (person.nickname = details.nickname);
+    details.fullName && (person.fullName = details.fullName);
+    details.prefix && (person.prefix = details.prefix);
+    details.suffix && (person.suffix = details.suffix);
+    details.note && (person.note = details.note);
+    
+    details.organization && (person.organization = details.organization);
+    details.department && (person.department = details.department);
+    details.jobTitle && (person.jobTitle = details.jobTitle);
+
+    details.URL && (person.URL = details.URL);
+    details.address && (person.address = details.address);
+    details.phone && (person.phone = details.phone);
+    details.email && (person.email = details.email);
+    details.instantMessage && (person.instantMessage = details.instantMessage);
+
+    Ti.Contacts.save();
+    return person;
+  }
 };
 
 include('lib/ique/roles/Analytics.js');
