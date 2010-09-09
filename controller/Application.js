469e49bb68008ad1d1ab2a1bb7d72587aa829928
aquilahgs <orlovsky@dev-ique.com>
2010-09-09 13:51:12 +0300

    Added support for non-tabs app layouts; first of all split layout for iPad

--- controller/Application.js
+++ controller/Application.js
@@ -3,7 +3,7 @@ Class('iQue.Application', {
   does: iQue.R.Logging
 
 , has: {
-    tabCtrl: { isPrivate: true, required: false, init: null }
+    view: { required: false, init: null }
   , layout: { required: false, init: null }
   }
 
@@ -14,25 +14,43 @@ Class('iQue.Application', {
     }
 
   , getWindow: function (name) {
-      return this.__tabCtrl.windows[name];
+      return this.view.iquePath('@' + name);
     }
   , createWindow: function (layout, params) {
       return this.openWindow(iQue.buildComponent(layout, params));
     }
   , openWindow: function (win) {
-      var origWin = this.__tabCtrl.getActiveWindow();
+      if (!this.hasTabsLayout()) {
+        var msg = "Application#openWindow function shouldn't be called for non-tab application layouts";
+        this.error(msg);
+        throw msg;
+      }
+      var origWin = this.view.getActiveWindow();
       var name = win.getName();
       Ti.Analytics.navEvent(origWin.getName(), name, 'nav.' + name, null);
       return this.getActiveTab().open(win.tiCtrl);
     }
     
   , getActiveTab: function () {
-      return this.__tabCtrl.getActiveTab();
+      if (!this.hasTabsLayout()) {
+        var msg = "Application#getActiveTab function shouldn't be called for non-tab application layouts";
+        this.error(msg);
+        throw msg;
+      }
+      return this.view.getActiveTab();
     }
   , setActiveTab: function (idx) {
-      return this.__tabCtrl.setActiveTab(idx);
+      if (!this.hasTabsLayout()) {
+        var msg = "Application#setActiveTab function shouldn't be called for non-tab application layouts";
+        this.error(msg);
+        throw msg;
+      }
+      return this.view.setActiveTab(idx);
     }
 
+  , loadLayout: function () {
+      return this.layouts.root;
+    }
   , loadData: function () {
       this.debug("Loading data");
       try {
@@ -47,20 +65,33 @@ Class('iQue.Application', {
   , loadApplication: function () {
       this.debug("Loading application interface");
       try {
-        this.layout = this.layouts.root;
-        this.__tabCtrl = iQue.buildComponent(this.layout, { });
-        this.__tabCtrl.open();
+        this.layout = this.loadLayout();
+        switch (this.layout.type) {
+          case 'split': this.layout.builder = iQue.UI.SplitView; break;
+          case 'tabs': this.layout.builder = iQue.UI.TabGroup; break;
+        }
+        this.view = iQue.buildComponent(this.layout, { });
+        this.view.open();
       } catch (ex) {
         this.error("Failed to load application interface, exception raised:");
         this.error(ex);
-        this.__tabCtrl = null;
+        this.view = null;
         return false;
       }
       return true;
     }
     
+  , hasTabsLayout: function () {
+      return this.layout.type == 'tabs';
+    }
+  , hadSplitLayout: function () {
+      return this.layout.type == 'split';
+    }
+  , hasCustomLayout: function () {
+      return this.layout.type != 'tabs' && this.layout.type != 'split';
+    }
   , iquePath: function (path) {
-      return this.__tabCtrl.iquePath(path);
+      return this.view.iquePath(path);
     }
   }
 });
