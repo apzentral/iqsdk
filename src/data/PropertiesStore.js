75966e2f0079b275311635f38a80ac3628da982c
aquilahgs <orlovsky@dev-ique.com>
2010-12-11 10:45:15 +0200

    PropertiesStore development

--- src/data/PropertiesStore.js
+++ src/data/PropertiesStore.js
@@ -23,16 +23,42 @@ iQ.data.PropertiesStore = new (Class({
       }
     }
 
-  , getArray: function (key) {
+  , getString: function (key, dflt) {
       try {
-        return Ti.App.Properties.getList(key, [ ]) || [ ];
+        return Ti.App.Properties.getString(key, dflt) || dflt;
       } catch (ex) {
         this.error("Exception during reading array key value from the store");
+        this.logException(ex);
+        return dflt;
+      }
+    }
+
+  , getArray: function (key, dflt) {
+      try {
+        return Ti.App.Properties.getList(key, dflt) || dflt;
+      } catch (ex) {
+        this.error("Exception during reading array key value from the store");
+        this.logException(ex);
+        return dflt;
+      }
+    }
+
+  , setString: function (key, str) {
+      str && (str = str.toString());
+      if (!isString(str)) {
+        var msg = "Can't save key to the store: the argument is not a string";
+        this.error(msg);
+        throw msg;
+      }
+      try {
+        Ti.App.Properties.setString(key, str);
+      } catch (ex) {
+        this.error("Exception during saving string key value to the store");
         this.logException(ex);
         return [ ];
       }
     }
-    
+
   , setArray: function (key, ary) {
       if (!isArray(ary)) {
         var msg = "Can't save key to the store: the argument is not an array";
