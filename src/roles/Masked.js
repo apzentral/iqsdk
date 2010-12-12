b48c2b05431773be46382727395f73197a6c3292
aquilahgs <orlovsky@dev-ique.com>
2010-12-12 09:59:22 +0200

    Masked UI role now operates configurable and customized masked view

--- src/roles/Masked.js
+++ src/roles/Masked.js
@@ -8,18 +8,13 @@ Role('iQ.role.UI.Masked', {
 , methods: {
     showMask: function () {
       if (!this.maskView) {
-        this.maskView = iQ.buildComponent({
-          builder: iQ.ui.Label,
-          config: {
-            top:0,left:0,right:0,bottom:0,
-            backgroundColor:'#80000000',
-            color:'white',font:{fontWeight:'bold'},
-            textAlign:'center',
-            text:this.maskMessage,
-            visible:false,
-            zIndex:1000
-          }
-        });
+        var layout = this.origConfig.mask;
+        layout.config = apply({
+          top: 0, left: 0, right: 0, bottom: 0,
+          visible: false,
+          zIndex: 1000
+        }, layout.config);
+        this.maskView = iQ.buildComponent(layout);
         this.add(this.maskView);
       }
       this.maskView.show();
