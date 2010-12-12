c562e7bb44dc95f9ff57a0a9b16a7a35695cf609
aquilahgs <orlovsky@dev-ique.com>
2010-12-12 20:25:17 +0200

    Masked views are now configurable

--- src/roles/Masked.js
+++ src/roles/Masked.js
@@ -3,24 +3,34 @@ Role('iQ.role.UI.Masked', {
   have: {
     maskState: false
   , maskView: null
-  , maskMessage: '%defaultMaskMessage'
   }
+  
+, after: {
+    render: function () {
+      this.renderMask();
+    }
+  }
+  
 , methods: {
-    showMask: function () {
-      if (!this.maskView) {
-        var layout = this.origConfig.mask;
-        layout.config = apply({
-          top: 0, left: 0, right: 0, bottom: 0,
-          visible: false,
-          zIndex: 1000
-        }, layout.config);
-        this.maskView = iQ.buildComponent(layout);
-        this.add(this.maskView);
-      }
-      this.maskView.show();
+    renderMask: function () {
+      //if (this.maskView) return;
+
+      var layout = this.origConfig.mask;
+      layout.config = apply({
+        top: 0, left: 0, right: 0, bottom: 0,
+        visible: false,
+        zIndex: 1000
+      }, layout.config);
+      
+      this.maskView = iQ.buildComponent(layout);
+      this.add(this.maskView);
+    }
+
+  , showMask: function () {
+      this.maskView && this.maskView.show();
     }
   , hideMask: function () {
-      this.maskView.hide();
+      this.maskView && this.maskView.hide();
     }
   }
 });
